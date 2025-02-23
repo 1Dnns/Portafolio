3ad76ab3ec8fa28d2d7c116efb11be1a26f15e7d
Dnnis <d.nelson_soto@hotmail.com>
2025-02-23 17:29:18 -0300

    frontend header

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -15,3 +15,33 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const professions = [
+        "Ingeniero Físico",
+        "Data Science",
+        "Técnico en Mecánica Automotriz y Autotrónica",
+    ];
+
+    const professionElement = document.querySelector('.lead');
+    let index = 0;
+
+    function animateProfessions() {
+        professionElement.textContent = professions[index];
+        professionElement.classList.remove('hidden');
+        professionElement.style.animation = 'typing 2s steps(40, end), blink-caret 0.75s step-end infinite';
+
+        setTimeout(() => {
+            professionElement.style.animation = 'erase 1s steps(40, end), blink-caret 0.75s step-end infinite'; // Animación de borrado
+            setTimeout(() => {
+                professionElement.classList.add('hidden');
+                index = (index + 1) % professions.length;
+                setTimeout(animateProfessions, 500); // Tiempo entre profesiones
+            }, 1000); // Duración de la animación de borrado
+        }, 3000); // Duración de la animación de escritura + tiempo que permanece
+    }
+
+    animateProfessions();
+});
